9ab90d65bf9a912ae422119a0ea8c67895b3ef33
Jared Vandeventer <vanthedev@gmail.com>
2020-05-31 13:27:50 -0600

    finished travis.yml config

--- config/ ci.js
+++ config/ ci.js
@@ -2,9 +2,9 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
-  redis: 'redis://127.0.0.1:6379'
+  redis: 'redis://127.0.0.1:6379';
 };
 
 // mongodb+srv://jared123:jared123@vanthedev-k2rxc.mongodb.net/blog_dev?retryWrites=true&w=majority
