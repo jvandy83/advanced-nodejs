e87886989b52db719239d2581c5679eefc81ebc3
Jared Vandeventer <vanthedev@gmail.com>
2020-07-23 15:42:37 -0600

    changed port number

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (!err) {
     console.log(`Listening on port`, PORT);
